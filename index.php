26a63985075c7f1bf6d3214873c02e99246ddb54
webnetkz <8gg001@gmail.com>
2019-11-19 16:45:31 +0600

    new style

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+    /*
     session_start();
 
     // Подключение БД
@@ -8,7 +8,8 @@
     $catSql = "SELECT * FROM `categories`;";
     $catRes = $pdo->query($catSql);
     $catRes = $catRes->fetchAll(PDO::FETCH_ASSOC);
-?>
+    */
+    ?>
 <!-- HTML Content -->
 <html>
     <head>
@@ -20,9 +21,11 @@
         <link rel="stylesheet" href="public/css/style.css">
         <link rel="stylesheet" href="public/css/mstyle.css">
         <link rel="shortcut icon" href="public/img/logo.png" type="image/png">
-        <!-- Description meta -->
+        <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+            <!-- Description meta -->
 
-        <!-- JavaScript -->
+            <!-- JavaScript -->
+            <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
     </head>
     <body>
     <!-- Append blocks -->
@@ -30,22 +33,102 @@
             <header>
                 <ul>
                     <li class="menuItem hoverBtn">
-                        <a href="index.php" class="nS">Главная</a>
+                        <a href="index.php" class="nS">Негізгі бет<br> Главная</a>
                     </li>
                     <li class="menuItem hoverBtn">
-                        <a href="archive.php" class="nS">Архив</a>
+                        <a href="archive.php" class="nS">Байкаулар<br> Конкурсы</a>
                     </li>
                     <li class="menuItem hoverBtn">
-                        <a href="about.php" class="nS">О проекте</a>
+                        <a href="archive.php" class="nS">Мұрағат<br> Архив</a>
                     </li>
                     <li class="menuItem hoverBtn">
-                        <a href="contact.php" class="nS">Контакты</a>
+                        <a href="about.php" class="nS">Өтінім үлгісі<br> Форма заявки</a>
+                    </li>
+                    <li class="menuItem hoverBtn">
+                        <a href="contact.php" class="nS">Байланыс<br> Контакты</a>
                     </li>
                 </ul>
             </header>
+             <pre>
 
 
-            <?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+             </pre>
+
+            <div class="fourImg">
+                <img src="public/img/f1.png" class="imgFour">
+                <img src="public/img/f2.png" class="imgFour">
+                <img src="public/img/f3.png" class="imgFour">
+                <img src="public/img/f4.png" class="imgFour">
+            </div>
+            <footer>
+                    <div class="hr"></div>
+                <h1 class="indexH1">НАШИ УЧАСТНИКИ</h1>
+
+                <!-- Flickity HTML init -->
+                <div class="carousel" data-flickity='{ "groupCells": true }'>
+                <div class="carousel-cell">
+                    <img src="public/img/scroll/1.png" class="imgScroll">
+                    <p class="pScrollB">Андрей Симонов</p>
+                    <p class="pScroll">12 лет</p>
+                </div>
+                <div class="carousel-cell">
+                    <img src="public/img/scroll/1.png" class="imgScroll" alt="">
+                    <p class="pScrollB">Андрей Симонов</p>
+                    <p class="pScroll">12 лет</p>
+                </div>
+                <div class="carousel-cell">
+                    <img src="public/img/scroll/1.png" class="imgScroll" alt="">
+                    <p class="pScrollB">Андрей Симонов</p>
+                    <p class="pScroll">12 лет</p>
+                </div>
+                <div class="carousel-cell">
+                    <img src="public/img/scroll/1.png" class="imgScroll" alt="">
+                    <p class="pScrollB">Андрей Симонов</p>
+                    <p class="pScroll">12 лет</p>
+                </div>
+                <div class="carousel-cell"></div>
+                <div class="carousel-cell"></div>
+                <div class="carousel-cell"></div>
+                <div class="carousel-cell"></div>
+                <div class="carousel-cell"></div>
+                <div class="carousel-cell"></div>
+                <div class="carousel-cell"></div>
+                </div>
+                <div class="hr"></div>
+            </footer>
+
+            <?php /*
             echo '<form action="" method="post">';        
                     if(!empty($catRes)) {
                         foreach($catRes as $k => $v) {
@@ -75,7 +158,7 @@
                 }
             }
 
-            ?>
+            */?>
         <!--<footer>
             2019 ©
         </footer>-->
